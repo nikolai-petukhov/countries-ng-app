77602ab533f9a0f117503cb1609259d3e4608e33
nikolai-petukhov <nikolaipetukhov@gmai.com>
2022-11-08 20:41:27 +0300

    create country component

--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-country',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountryComponent implements OnInit {
 
+  @Input() country: any = null;
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.country)
   }
 
 }
