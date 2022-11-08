03e7d7918553adb77cce17c88dc812d4a75958c7
nikolai-petukhov <nikolaipetukhov@gmai.com>
2022-11-08 17:06:33 +0300

    create filter component and data binding

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { FilterComponent } from './filter/filter.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FilterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/filter/filter.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-filter',
+  templateUrl: './filter.component.html',
+  styleUrls: ['./filter.component.css']
+})
+export class FilterComponent implements OnInit {
+
+  @Input() filterInputValue!: string;
+  @Output() filterInputValueChange = new EventEmitter<string>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  filterInputHandler(searchValue: string): void {
+    this.filterInputValue = searchValue;
+    this.filterInputValueChange.emit(this.filterInputValue);
+    console.log(this.filterInputValue);//
+  }
+
+}
